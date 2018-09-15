1d50f51e11c77a887cffc6cb0aab4ee0aebcbde3
Jay Piamjariyakul <j.piamjariyakul@outlook.com>
2018-09-16 00:50:54 +0700

    Added image to gallery
    Also added blog entry

--- js/pageResponseNav.js
+++ js/pageResponseNav.js
@@ -1,7 +1,8 @@
 function pageResponseNav()
 {
+    // Obtains element called "navbar"
     var element = document.getElementById("navbar");
-    if (element.className === "navbar")
+    if (element.className == "navbar")
     {
         element.className += " responsive";
     }
