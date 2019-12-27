c8f6410d75cdacde86ebce2207e2f8d8979831c3
Jay Piamjariyakul <j.piamjariyakul@outlook.com>
2019-12-27 04:37:03 +0000

    Applied BUILD content to main repo

---
+++ precache-manifest.28a56e40fc4b4c21b5186f45aa877529.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2f3961295aa1d9faff91f2e930286f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d513eb7dd248cb5bbd9",
+    "url": "/static/css/main.9c27f301.chunk.css"
+  },
+  {
+    "revision": "bb88f910b5ba3f7cf967",
+    "url": "/static/js/2.e4377b5f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.e4377b5f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9d513eb7dd248cb5bbd9",
+    "url": "/static/js/main.46d4435b.chunk.js"
+  },
+  {
+    "revision": "66e539bb4b9122fea9b0",
+    "url": "/static/js/runtime-main.0d31feeb.js"
+  }
+]);
